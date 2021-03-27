a621c90b3941b99214b6853988f2308767a798fb
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-03-27 14:36:20 -0400

    added userId to create method

--- ElevenNoteRedBadge.WebMVC/Controllers/NoteController.cs
+++ ElevenNoteRedBadge.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,12 @@ namespace ElevenNoteRedBadge.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            //var model = new NoteListItem[0];
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+
             return View();
         }
 
@@ -29,9 +36,15 @@ namespace ElevenNoteRedBadge.WebMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                return View(model);
             }
-            return View(model);
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
