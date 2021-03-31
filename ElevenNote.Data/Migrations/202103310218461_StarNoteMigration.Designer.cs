5bee113380dff6aae1b3c79342c92da91dd2e4ee
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-03-31 00:18:08 -0400

    Added Javascript for Starred Functionality

--- ElevenNote.Data/Migrations/202103310218461_StarNoteMigration.Designer.cs
+++ ElevenNote.Data/Migrations/202103310218461_StarNoteMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace ElevenNote.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.0-30225")]
-    public sealed partial class InitialMigration : IMigrationMetadata
+    public sealed partial class StarNoteMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(StarNoteMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "202103271743430_Initial Migration"; }
+            get { return "202103310218461_StarNoteMigration"; }
         }
         
         string IMigrationMetadata.Source
