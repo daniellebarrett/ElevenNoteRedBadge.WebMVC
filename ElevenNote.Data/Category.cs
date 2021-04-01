7db903a5d2f236b0bdcce6a47a02b6ef89d8b730
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-04-01 18:26:34 -0400

    Added Category and CategoryListItem

---
+++ ElevenNote.Data/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+   public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [Display(Name = "Your Category")]
+        public string Name { get; set; }
+        [Required]
+        public DateTimeOffset CreateUtc { get; set; }
+        [Required]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
---
+++ ElevenNoteRedBadge.WebMVC/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNoteRedBadge.WebMVC.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var model = new CategoryListItem[0];
+            return View();
+        }
+    }
+}
